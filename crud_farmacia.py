d06900e572a53373a4c00ed9f2710d5d679bae0c
LucasMariz <lucasmarizmaia@hotmail.com>
2024-03-10 21:46:40 -0300

    Criando classe GerenciadorDeBanco

--- crud_farmacia.py
+++ crud_farmacia.py
@@ -1,4 +1,24 @@
 import sqlite3
 
-def conectar_banco():
-    return sqlite3.connect('farmacia.db')
+class GerenciadorDeBanco:
+    def __init__(self, db_path='farmacia.db'):
+        self.db_path = db_path
+        self.conectar_banco()
+        self.criar_tabelas()
+
+    def conectar_banco(self):
+        self.conn = sqlite3.connect(self.db_path)
+        return self.conn
+
+    def criar_tabelas(self):
+        cursor = self.conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS clientes (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            nome TEXT NOT NULL,
+                            email TEXT UNIQUE)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS produtos (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            nome TEXT NOT NULL,
+                            quantidade INTEGER,
+                            preco DECIMAL)''')
+        self.conn.commit()
